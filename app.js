4cae7fdebd1c111d0dfdd4bc3845042af49db815
Loris Caputo <lorixyes@gmail.com>
2023-10-21 01:48:44 +0200

    changes

--- app.js
+++ app.js
@@ -6,16 +6,16 @@ const cors = require('cors');
 app.use(bodyParser.json());
 app.use(cors()); 
 
-const placesRoutes = require('./routes/places');
-const usersRoutes = require('./routes/users');
+//const placesRoutes = require('./routes/places');
+//const usersRoutes = require('./routes/users');
  
 app.get('/', (req, res) => {
   res.json({ message: 'API di esempio su Vercel!' });
 });  
 
-app.use('/api', placesRoutes);
-
-app.use('/users', usersRoutes); 
+//app.use('/api', placesRoutes);
+ 
+//app.use('/users', usersRoutes); 
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
