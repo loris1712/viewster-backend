3b55d346c844057464a408d06b2e5e572a731956
Loris Caputo <lorixyes@gmail.com>
2023-10-21 01:26:59 +0200

    changes

--- app.js
+++ app.js
@@ -1,26 +1,21 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const cors = require('cors'); // Aggiungi questa riga per importare il modulo CORS
+const cors = require('cors'); 
 
 app.use(bodyParser.json());
-app.use(cors()); // Usa il middleware CORS per abilitare le richieste da tutti gli origini
+app.use(cors()); 
 
 const placesRoutes = require('./routes/places');
 const usersRoutes = require('./routes/users');
-
+ 
 app.get('/', (req, res) => {
   res.json({ message: 'API di esempio su Vercel!' });
 });  
 
-// Utilizza le rotte per le chiamate API relative ai luoghi
 app.use('/api', placesRoutes);
 
-// Utilizza le rotte per le chiamate API relative agli utenti
 app.use('/users', usersRoutes); 
-
-// Altre configurazioni e middleware dell'app Express
-// ...
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
